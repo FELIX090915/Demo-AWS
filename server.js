f1e36bfdd318c9e4386e10794dd1efdf1395d21c
FELIX090915 <felix090915@gmail.com>
2021-10-11 15:07:09 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = requiere("http");
+
+const port = 3000;
+
+const server = htpp.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hello World")
+});
+
+server.listen(port, () => {
+    console.log("Server running on port: ${port}")
+});
